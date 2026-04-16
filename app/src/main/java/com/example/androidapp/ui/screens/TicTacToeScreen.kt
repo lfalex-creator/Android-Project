3518b50424df8a2457d3d5b7061a0331541fa741
ALF <florin.lupu@student.unitbv.ro>
2026-04-16 21:24:21 +0300

    (Biscotti) TicTacToe game is finished. Will revisit in order to change implementation according to MVVM standards.

--- app/src/main/java/com/example/androidapp/ui/screens/TicTacToeScreen.kt
+++ app/src/main/java/com/example/androidapp/ui/screens/TicTacToeScreen.kt
@@ -1,19 +1,25 @@
 package com.example.androidapp.ui.screens
 
-import android.R
 import android.app.AlertDialog
 import android.content.Context
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,19 +32,35 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 
 
-var matr =arrayOf(
+var matr=arrayOf(
     charArrayOf(' ', ' ', ' '),
     charArrayOf(' ', ' ', ' '),
     charArrayOf(' ', ' ', ' ')
 )
-
+val playerOneColour = Color.Red
+val playerTwoColour = Color.Blue
+var playerOneSymb=' '
+var playerTwoSymb=' '
 @Composable
     fun TicTacToeScreen() {
         var playerOneTurn by remember { mutableStateOf(true) }
         val context=LocalContext.current
+    CustomDialogExample()
+    DisposableEffect(Unit) {
 
+
+
+        onDispose {
+            matr=arrayOf(
+                charArrayOf(' ', ' ', ' '),
+                charArrayOf(' ', ' ', ' '),
+                charArrayOf(' ', ' ', ' ')
+            )
+        }
+    }
 
         Column(
             modifier = Modifier.fillMaxSize(),
@@ -53,10 +75,15 @@ var matr =arrayOf(
                             col = col,
                             playerOneTurn = playerOneTurn,
                             onMove = {
-                                matr[row][col] = if (playerOneTurn) 'X' else 'O'
+                                matr[row][col] = if (playerOneTurn) playerOneSymb else playerTwoSymb
                                 if(CheckWinner(matr))
                                 {
                                     ShowWinner(context, playerOneTurn)
+
+                                    val temp= playerOneSymb
+                                    playerOneSymb = playerTwoSymb
+                                    playerTwoSymb = temp
+
                                     Reset(matr)
                                 }
                                 playerOneTurn = !playerOneTurn
@@ -65,6 +92,13 @@ var matr =arrayOf(
                     }
                 }
             }
+            Spacer(modifier = Modifier.height(50.dp))
+            Text(
+                text = (if (playerOneTurn) "Player 1's" else "Player 2's") + " turn" ,
+                fontSize = 20.sp,
+                color = if (playerOneTurn) playerOneColour else playerTwoColour,
+                textAlign = TextAlign.Center
+            )
         }
     }
     fun ShowWinner(context: Context,playerOneTurn: Boolean) {
@@ -116,7 +150,7 @@ var matr =arrayOf(
                 .border(1.dp, Color.Black)
                 .clickable {
                     if (matr[row][col] == ' ') {
-                        currentColour = if (playerOneTurn) Color.Red else Color.Blue
+                        currentColour = if (playerOneTurn) playerOneColour else playerTwoColour
                         onMove()
                     }
                 }
@@ -133,6 +167,82 @@ var matr =arrayOf(
             )
         }
     }
+
+@Composable
+fun CustomDialogExample() {
+    var showDialog by remember { mutableStateOf(true) }
+
+    if (showDialog) {
+        Dialog(onDismissRequest = { showDialog = false }) {
+            Box(
+                modifier = Modifier
+                    .background(Color.White)
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                )
+                {
+                    Text(
+                        text = "Player 1, please pick a symbol",
+                        fontSize = 20.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+                    Spacer(modifier = Modifier.height(50.dp))
+                    Row {
+                        Spacer(modifier = Modifier.width(10.dp))
+                        Box(
+                            modifier = Modifier
+                                .border(1.dp, Color.Black)
+                                .clickable {
+                                    playerOneSymb='X'
+                                    playerTwoSymb='O'
+                                    showDialog=false
+                                }
+                                .size(110.dp, 110.dp),
+                            contentAlignment = Alignment.Center
+                        )
+                        {
+                            Text(
+                                text = "X",
+                                modifier = Modifier.fillMaxSize(),
+                                fontSize = 100.sp,
+                                color = playerOneColour,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                        Spacer(modifier = Modifier.width(50.dp))
+                        Box(
+                            modifier = Modifier
+                                .border(1.dp, Color.Black)
+                                .clickable {
+                                    playerOneSymb='O'
+                                    playerTwoSymb='X'
+                                    showDialog=false
+                                }
+                                .size(110.dp, 110.dp),
+                            contentAlignment = Alignment.Center
+                        )
+                        {
+                            Text(
+                                text = "O",
+                                modifier = Modifier.fillMaxSize(),
+                                fontSize = 100.sp,
+                                color = playerOneColour,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                        Spacer(modifier = Modifier.width(10.dp))
+
+                    }
+                    Spacer(modifier = Modifier.height(30.dp))
+                }
+
+            }
+        }
+    }
+}
 
     @Preview(showBackground = true)
     @Composable
